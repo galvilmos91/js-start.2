569c47bdebd3dbf5bfbe1537c9cc301d583085c2
BlackCoconut <gal.vilmos.91@gmail.com>
2026-03-01 21:31:44 +0100

    2.7

--- demo.js
+++ demo.js
@@ -10,8 +10,61 @@ form.addEventListener('submit', onSubmit);
 function onSubmit(ev) {
     ev.preventDefault();
 
-const li = document.createElement('li');
-li.appendChild(document.createTextNode(`${nameInput.value} ${emailInput.value}`));
+    if (nameInput.value === '') {
+        
+        message.classList.add('error');
+        message.innerHTML = 'Helytelen felhasználónév! Süss egy csokis sütit!';
+        setTimeout(() => {
+            message.classList.remove('error');
+            message.innerHTML = '';
+        } , 3000);
+        
+        }
 
-userList.appendChild(li);
+    else if(emailInput.value === '' || !emailInput.value.includes('@')){
+        message.classList.add('error');
+        message.innerHTML = 'Hibás email';
+        setTimeout(() => {
+            message.classList.remove('error');
+            message.innerHTML = '';
+        } , 3000);
+    }
+
+    else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} ${emailInput.value} ${roleInput.value}`));
+
+        userList.appendChild(li);
+
+        nameInput.value = '';
+        emailInput.value = ''; 
+       
+    }
 }
+/* function clearError() {
+    setTimeout(() => {
+            message.classList.remove('error');
+            message.innerHTML = '';
+            }, 3000)
+} */
+
+    /* if(nameInput.value === '') {
+    alert('üres név mező');
+    }
+    else if(emailInput.value === '' || !emailInput.value.includes('@')) 
+        
+    alert('hibás email cím');
+    
+{    message.classList.add('error');
+     message.innerHTML = 'Hibás adatok';
+     }
+
+    else {
+        const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value} ${emailInput.value}`));
+
+    userList.appendChild(li);
+
+    nameInput.value = '';
+    emailInput.value = ''; 
+    }; */
